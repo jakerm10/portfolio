09226ed0e29754d9db1f9fd4470a6a1e5d33a2cc
Jake Moore <jamo7788@colorado.edu>
2026-05-08 00:54:43 -0600

    langing page image

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,10 @@
+import Webbie from "../assets/images/Webbie.png";
+import "../css/Landing.css";
+
 export default function Landing(){
     return(
         <section className="Landing">
+            <img src={Webbie} alt="Jake Moore" className="webbie" />
             <h1>Jake Moore</h1>
         </section>
     );
