7e0a8150be444342bd2aaa6de1f5c7b859a73426
Jake Moore <jamo7788@colorado.edu>
2026-05-10 17:48:42 -0600

    working text, missing overlay

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,22 +1,27 @@
 import Webbie from "../assets/images/Webbie.jpeg";
 import "../css/Landing.css";
+import { useEffect, useRef, useState } from "react";
 
 export default function Landing(){
+    const imageRef = useRef(null);
+    const [clip, setClip] = useState("50%");
+
+    useEffect(() => {
+        setClip("50%");
+      }, []);
+
     return(
         <section className="landing">
-            <div className="imagetextcontainer">
+            <div className="stack">
                 <img src={Webbie} alt="Jake Moore" className="webbie" />
                 <div className="text-wrapper">
                     <div className="landing-text">
-                        Jake Moore
+                        Jake<br></br>Moore
                     </div>
-                    <div className="landing-overlay">
-                       Jake Moore
+                    <div className="landing-text landing-overlay" style={{clipPath: `inset(0 ${clip} 0 0)`}}>
+                       Jake<br></br>Moore
                     </div>
                 </div>
-                
-            
-                
             </div>
             
         </section>
