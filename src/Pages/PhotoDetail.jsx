306b92892641560ae9af5741b6d89c022634cf8f
Jake Moore <jamo7788@colorado.edu>
2026-05-24 15:06:56 -0600

    created anya page, protected images

--- src/Pages/PhotoDetail.jsx
+++ src/Pages/PhotoDetail.jsx
@@ -7,10 +7,17 @@ export default function PhotoDetail() {
     const photo = photoData.find(p => p.id === id);
 
     return (
-        <div>
-            <img src={photo.image} className="mainimage" />
-            <h1>{photo.title}</h1>
-            <p>{photo.description}</p>
+        <div className="box">
+            <div>
+                <img src={photo.image} className="mainimage" />
+            </div>
+            <div>
+                <h1>{photo.title}</h1>
+                <h2>{photo.event}, {photo.date}</h2>
+                <h3>{photo.location}</h3>
+                <p>{photo.description}</p>
+            </div>
+            
         </div>
     );
 }
--- src/data/photoData.js
+++ src/data/photoData.js
@@ -1,5 +1,6 @@
 import { vertsf, horizsf } from "../components/Photos.js";
 
 export const photoData = [
-    { id: 'anya-nelson', image: vertsf[57], title: 'Anya Nelson', description: '...', category: 'Portrait' },
+    { id: 'anya-nelson', image: vertsf[57], title: 'Anya Nelson', event:'Colorado VS Iowa State', location:' CU South Campus', date:'2/28/25', 
+        description: 'I captured this shot as Anya was returning a serve. Seated near the player\'s rest area I tried to get a clean shot of her face. I noticed on the first return that the front player\'s racket was in the way of her face. Instead of moving myself to eliminate the foreground, I tried to incorperate the racket to add some unique framing. When Anya returned the next serve, she was framed perfectly within the racket leading to this awesome shot. '},
 ]
