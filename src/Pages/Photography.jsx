4a69f157777388cc998b712152c7a920f367d87c
Jake Moore <jamo7788@colorado.edu>
2026-05-23 19:19:28 -0600

    added overlay to all imaghes

--- src/Pages/Photography.jsx
+++ src/Pages/Photography.jsx
@@ -5,7 +5,6 @@ import "../css/Photography.css";
 export default function Photography() {
     return (
         <div className="gridimages">
-
             <div className="imgcontainer">
                 <img className="images" src={vertsf[57]} alt="Anya Nelson" />
                 <div className="textbox">
@@ -34,41 +33,235 @@ export default function Photography() {
                 </div>
             </div>
 
-            <img className="images" src={vertsf[31]} alt="" />
-            <img className="images" src={vertsf[33]} alt="" />
-            <img className="images" src={vertsf[3]} alt="" />
-            <img className="images" src={horizsf[3]} alt="" />
-            <img className="images" src={horizsf[0]} alt="" />
-            <img className="images" src={horizsf[1]} alt="" />
-            <img className="images" src={vertsf[4]} alt="" />
-            <img className="images" src={vertsf[8]} alt="" />
-            <img className="images" src={vertsf[10]} alt="" />
-            <img className="images" src={vertsf[11]} alt="" />
-            <img className="images" src={vertsf[12]} alt="" />
-            <img className="images" src={vertsf[13]} alt="" />
-            <img className="images" src={vertsf[16]} alt="" />
-            <img className="images" src={vertsf[20]} alt="" />
-            <img className="images" src={vertsf[21]} alt="" />
-            <img className="images" src={vertsf[23]} alt="" />
-            <img className="images" src={vertsf[24]} alt="" />
-            <img className="images" src={vertsf[26]} alt="" />
-            <img className="images" src={vertsf[27]} alt="" />
-            <img className="images" src={vertsf[29]} alt="" />
-            <img className="images" src={vertsf[30]} alt="" />
-            <img className="images" src={vertsf[35]} alt="" />
-            <img className="images" src={vertsf[39]} alt="" />
-            <img className="images" src={vertsf[40]} alt="" />
-            <img className="images" src={vertsf[42]} alt="" />
-            <img className="images" src={vertsf[43]} alt="" />
-            <img className="images" src={vertsf[44]} alt="" />
-            <img className="images" src={vertsf[45]} alt="" />
-            <img className="images" src={vertsf[48]} alt="" />
-            <img className="images" src={vertsf[49]} alt="" />
-            <img className="images" src={vertsf[51]} alt="" />
-            <img className="images" src={vertsf[56]} alt="" />
-            <img className="images" src={vertsf[58]} alt="" />
-            <img className="images" src={vertsf[0]} alt="" />
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[31]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
 
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[33]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[3]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[0]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[1]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[4]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[8]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[10]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[11]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[12]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[13]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[16]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[20]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[21]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[23]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[24]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[26]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[27]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[29]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[30]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[35]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[39]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[40]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[42]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[43]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[44]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[45]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[48]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[49]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[51]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[56]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[58]} alt="" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
+            <div className="imgcontainer">
+                <img className="images" src={vertsf[0]} alt="Anya Nelson" />
+                <div className="textbox">
+                    <div className="text">Anya Nelson</div>
+                </div>
+            </div>
         </div>
     );
 }
