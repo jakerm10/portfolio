679bdd85606a8aae0c8af07103d362edb4a5fee9
Jake Moore <jamo7788@colorado.edu>
2026-06-18 11:31:15 -0600

    change verts

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -17,6 +17,10 @@
 // export const horizsf = Object.fromEntries(
 //     Object.entries(horizontalfulls).map(([path, url]) => [path.split('/').pop(), url])
 // );
+const verticalthumbs = import.meta.glob('../assets/images/Thumbs/Verts/*', { eager: true, as: 'url' });
+const verticalfulls = import.meta.glob('../assets/images/Fulls/Verts/*', { eager: true, as: 'url' });
+const horizontalthumbs = import.meta.glob('../assets/images/Thumbs/Horiz/*', { eager: true, as: 'url' });
+const horizontalfulls = import.meta.glob('../assets/images/Fulls/Horiz/*', { eager: true, as: 'url' });
 export const vertst = Object.fromEntries(
     Object.entries(verticalthumbs).map(([path, mod]) => [
         path.split('/').pop(),
