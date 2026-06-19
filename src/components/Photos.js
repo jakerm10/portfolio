aac803b79502b631d002d42d6707a99a340e23d0
Jake Moore <jamo7788@colorado.edu>
2026-06-18 23:01:39 -0600

    portraits added

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,7 +1,7 @@
-const verticalthumbs = import.meta.glob('../assets/images/Thumbs/Verts/*', { eager: true, as: 'url' });
-const verticalfulls = import.meta.glob('../assets/images/Fulls/Verts/*', { eager: true, as: 'url' });
-const horizontalthumbs = import.meta.glob('../assets/images/Thumbs/Horiz/*', { eager: true, as: 'url' });
-const horizontalfulls = import.meta.glob('../assets/images/Fulls/Horiz/*', { eager: true, as: 'url' });
+const verticalthumbs = import.meta.glob('../assets/images/Thumbs/Verts/*', { eager: true, query: '?url', import: 'default' });
+const verticalfulls = import.meta.glob('../assets/images/Fulls/Verts/*', { eager: true, query: '?url', import: 'default' });
+const horizontalthumbs = import.meta.glob('../assets/images/Thumbs/Horiz/*', { eager: true, query: '?url', import: 'default' });
+const horizontalfulls = import.meta.glob('../assets/images/Fulls/Horiz/*', { eager: true, query: '?url', import: 'default' });
 
 export const vertst = Object.fromEntries(
     Object.entries(verticalthumbs).map(([path, url]) => [
